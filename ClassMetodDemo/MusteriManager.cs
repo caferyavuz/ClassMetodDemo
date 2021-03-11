96db19a1e531f27599ebc252033df51bfa528876
caferyavuz <caferyavuz134@gmail.com>
2021-03-11 20:48:58 +0300

    11 Ocak Ödev Sonu

---
+++ ClassMetodDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetodDemo
+{
+    public class MusteriManager
+    {
+        List<Musteri> musteriler;
+        public MusteriManager()
+        {
+            musteriler = new List<Musteri>();
+        }
+        public void MusteriEkle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} isimli müşteri eklendi.");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine($"{musteri.Ad} {musteri.Soyad} isimli müşteri silindi.");
+        }
+
+        public void MusteriListele()
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine($"{musteri.Id} - {musteri.Ad} - {musteri.Soyad}");
+            }
+        }
+    }
+}
--- ClassMetodDemo/Program.cs
+++ ClassMetodDemo/Program.cs
@@ -6,7 +6,30 @@ namespace ClassMetodDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Recep Cafer";
+            musteri1.Soyad = "Yavuz";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Anıl";
+            musteri2.Soyad = "Öztürk";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Mehmet";
+            musteri3.Soyad = "Can";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
+            musteriManager.MusteriSil(musteri2);
+
+            musteriManager.MusteriListele();
         }
     }
 }
